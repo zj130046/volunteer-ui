ad3efff3dd43f54e2b6037801b2498db0ede4994
littlespring123 <3198662089@qq.com>
2023-11-15 16:40:53 +0800

    feat: 打包分环境

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     //publicPath: '/wustvolunteer/', //公共路径
     publicPath: './', //公共路径
-    outputDir: 'volunteer', //输出文件目录
+    outputDir: process.env.VUE_APP_ENV ==='production'?'volunteer':'volunteer-test', //输出文件目录
     assetsDir: 'static', //静态资源文件名称
     filenameHashing: false, //文件名是否包含hash
     lintOnSave: false, //是否使用eslint
